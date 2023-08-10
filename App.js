3e2c8b2dedfbda2815f42c60644b9ef49d8a8b75
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2023-08-10 19:01:54 +0800

    passed all test

---
+++ App.js
@@ -0,0 +1,48 @@
+function App() {
+  const [quote, setQuote] = React.useState("");
+  const [author, setAuthor] = React.useState("");
+
+  React.useEffect(() => {
+    getNewQuote();
+  }, []);
+
+  const getNewQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      });
+  };
+
+  return (
+    <div className="App">
+      <div id="quote-box" className="card q-box">
+        <div id="text">{quote}</div>
+        <div id="author">{author}</div>
+        <button>
+          <a
+            id="tweet-quote"
+            className="btn"
+            target="_blank"
+            href={
+              "https://twitter.com/intent/tweet?&related=freecodecamp&text=" +
+              encodeURIComponent('"' + quote + '" ' + "- " + author)
+            }
+          >
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+        </button>
+        <button id="new-quote" onClick={getNewQuote}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
